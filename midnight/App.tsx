3172d6c714a3f7051e0fa7cd3d79b11bc9f5272c
Mamieo <mamieorine@gmail.com>
2020-04-11 22:31:52 +0700

    Add navigation tabs

--- midnight/App.tsx
+++ midnight/App.tsx
@@ -1,19 +1,63 @@
+//import 'react-native-gesture-handler'; // built-in touch system. if didn't import it, app may crash in production.
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+// manages navigation tree and navigation state.
+import { NavigationContainer } from '@react-navigation/native'; 
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+import HomeScreen from './src/screens/HomeScreen';
+import DashboardScreen from './src/screens/DashboardScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
+type IconState = {
+  active: string;
+  Inactive: string;
+};
+
+const Tab = createBottomTabNavigator();
+
+const icons = {
+  iconHome: {
+    active: 'ios-information-circle',
+    Inactive: 'ios-information-circle-outline'
+  },
+  iconDashboard: {
+    active: 'ios-list-box',
+    Inactive: 'ios-list'
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const iconHome: IconState = icons.iconHome;
+const iconDashboard: IconState = icons.iconDashboard;
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator 
+        initialRouteName="Home"
+        tabBarOptions={{
+          activeTintColor: 'brown',
+          inactiveTintColor: 'gray'
+        }}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size}) => {
+            let iconName;
+
+            if (route.name === 'Home') {
+              iconName = focused ? iconHome.active : iconHome.Inactive;
+            } 
+            else if (route.name === 'Dashboard') {
+              iconName = focused ? iconDashboard.active : iconDashboard.Inactive;
+            }
+
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          }
+        })}>
+
+        <Tab.Screen name="Home" component={HomeScreen} options={{ title: 'HOME' }} />
+        <Tab.Screen name="Dashboard" component={DashboardScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
+}
+
+export default App;
---
+++ midnight/src/screens/HomeScreen/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { View, Text, Button, StyleSheet, ViewStyle } from 'react-native';
+import { StackNavigationProp } from '@react-navigation/stack';
+
+type RootStacks = {
+  Dashboard: undefined;
+};
+
+interface Props {
+  navigation: StackNavigationProp<RootStacks>;
+}
+
+interface Styles {
+  containerHome: ViewStyle;
+}
+
+export default class HomeScreen extends React.Component<Props> {
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <View style={styles.containerHome}>
+        <Text>This is Home Screen na ja</Text>
+        <Button title="Go to dashboard" onPress={() => this.props.navigation.navigate('Dashboard')}/>
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create<Styles>({
+  containerHome: {
+      flex: 1,
+      backgroundColor: 'tomato',
+      alignItems: 'center',
+      justifyContent: 'center',
+  },
+})
